97a69eabd21282d5252227884d8a164de2056b4b
Beyy93 <beydi.coulibaly2408@gmail.com>
2024-10-21 09:21:20 +0000

    2.3

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-	char *stce = "Programming is like building a multilingual puzzle\n";
+	char stce[] = "Programming is like building a multilingual puzzle\n";
 
 	puts(stce);
 	return (0);
