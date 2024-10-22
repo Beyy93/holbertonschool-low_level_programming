42e3c7415cf205c590e404af618cae8336b1381a
Beyy93 <beydi.coulibaly2408@gmail.com>
2024-10-22 09:06:03 +0000

    1.3

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -11,7 +11,7 @@
 int main(void)
 {
 	int n;
-	signed int last_dig;
+	int last_dig;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
@@ -22,7 +22,7 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, last_dig);
 	}
-	else if (n == 0)
+	else if (last_dig == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, last_dig);
 	}
