ac95b44bf3b0535c3f417eec002233aa20d386a5
Beyy93 <beydi.coulibaly2408@gmail.com>
2024-10-22 07:54:31 +0000

    0.2

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -19,13 +19,13 @@ int main(void)
 	{
 		printf("is positive\n");
 	}
-	else if (n == 0)
+	else if (n < 0)
 	{
-		printf ("is zezro\n");
+		printf ("is negative\n");
 	}
 	else
 	{
-		printf ("is negative\n");
+		printf ("is zero\n");
 	}
 	return (0);
 }
