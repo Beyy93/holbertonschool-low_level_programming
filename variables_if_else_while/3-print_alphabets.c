08851e626d990c78d9c2cd2c7310e0415399f4a0
Beyy93 <beydi.coulibaly2408@gmail.com>
2024-10-22 12:39:00 +0000

    y

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char i;
+	i = 'a';
+
+	while (i <= 'z')
+	{
+		putchar(i);
+		i++;
+	}
+	i = 'a';
+
+	while (i <= 'z')
+	{
+		putchar(i - 32);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
