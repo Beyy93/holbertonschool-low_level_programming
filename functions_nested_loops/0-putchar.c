b459faf6996dc28ce0f70730818d4a252e21598c
Beyy93 <beydi.coulibaly2408@gmail.com>
2024-10-23 12:43:44 +0000

    yessiiir

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	write(1, "_putchar", 8);
+	write(1, "\n", 1);
+	return (0);
+}
