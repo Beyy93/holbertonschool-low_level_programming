24f1dc3b0b513e35fc0497211923b49677fc86b4
Beyy93 <beydi.coulibaly2408@gmail.com>
2024-10-23 13:10:52 +0000

    content

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,13 +1,12 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * main - Entry point
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: Always 0 (Success)
  */
-int _putchar(char c)
+
+void print_alphabet()
 {
-	return (write(1, &c, 1));
+	write(1, "abcdefghijklmnopqrstuvwxyz", 26);
 }
