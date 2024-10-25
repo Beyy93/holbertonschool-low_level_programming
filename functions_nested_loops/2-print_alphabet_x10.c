8971cd3b357478bbebe6d46a75276cff3ca079d9
Beyy93 <beydi.coulibaly2408@gmail.com>
2024-10-25 09:55:07 +0000

    yess

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * print_alphabet_10x - Entry point
+ *
+ * Return: Always void (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		char c;
+
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		if (i != 9)
+		{
+			_putchar('\n');
+		}
+	}
+}
