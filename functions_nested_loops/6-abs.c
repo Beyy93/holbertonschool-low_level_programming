f7c41d9222bd9fc24cf4c917811833458d5ff110
Beyy93 <beydi.coulibaly2408@gmail.com>
2024-10-26 21:02:49 +0000

    ?

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+int _abs(int)
+{
+	int n;
+
+	n = 0;
+
+	int _abs(int)
+	{
+		if (n == 0)
+		{
+			return(0);
+		}
+		else if (n < 0)
+		{
+			n = n * (-1);
+			return (n);
+		}
+		else
+		{
+			return (n);
+		}
+	}
+}
