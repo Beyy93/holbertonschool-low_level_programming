493aec4caf494e705344a1c1a3e50f024c26ab50
Beyy93 <beydi.coulibaly2408@gmail.com>
2024-10-28 22:25:55 +0000

    6.0

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_line - draw a straight line
+ * @n: number of times underscore is printed
+ * Description: Can only use _putchar to print
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	i = 0;
+
+	if (n > 0)
+	{
+		while (i <= n)
+		{
+			_putchar('_');
+			i++;
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
