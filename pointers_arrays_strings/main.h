1d5f431863e7f0e0fe832529cf6554ddaca06595
Beyy93 <beydi.coulibaly2408@gmail.com>
2024-10-29 13:35:56 +0000

    yessiir

---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _putchar(char c);
+int _isalpha(char c);
+int print_sign(int n);
+int _abs(int x);
+int print_last_digit(int n);
+void jack_bauer(void);
+void reset_to_98(int *n);
+#endif
