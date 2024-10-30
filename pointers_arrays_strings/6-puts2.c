7a574143c2c117f4e31a499544c8c4761ce42b56
Beyy93 <beydi.coulibaly2408@gmail.com>
2024-10-30 13:33:33 +0000

    yes!!

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -8,12 +8,20 @@
 void puts2(char *str)
 {
 	int i = 0;
+	int a = 2;
 
 	while (str[i])
 	{
-		_putchar(str[i]);
-		i++;
-		i++;
+		if (a == 2)
+		{
+			_putchar(str[i]);
+			a = 0;
+		}
+		else
+		{
+			a++;
+			i++;
+		}
 	}
 	_putchar('\n');
 }
