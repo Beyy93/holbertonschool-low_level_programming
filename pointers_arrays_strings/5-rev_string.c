95520f6978e8a3a2699bac579d40e75066048fc9
Beyy93 <beydi.coulibaly2408@gmail.com>
2024-11-02 16:46:36 +0000

    YES !

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -7,3 +7,25 @@
 
 void rev_string(char *s)
 {
+	char *debut;
+	char *fin;
+	char temp;
+
+	debut = &s[0];
+	fin = &s[0];
+	temp = *debut;
+
+	while (*fin != 0)
+	{
+		fin++;
+	}
+	fin--;
+	while (fin > debut)
+	{
+		temp = *debut;
+		*debut = *fin;
+		*fin = temp;
+		debut++;
+		fin--;
+	}
+}
